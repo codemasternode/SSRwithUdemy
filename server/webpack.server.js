cf27e20410653904f1b9474ed76a88de4b4ad2f8
codemasternode <marcinwarzybok@outlook.com>
2018-06-30 22:45:21 +0200

    bundle.js

---
+++ server/webpack.server.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+    //Inform webpack that we are building bundle for nodeJS
+    // rather than for the browser
+    target: 'node',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        'react',
+                        'stage-0',
+                        ['env', { targets: { browsers: ['last 2 versions'] } }]
+                    ]
+                }
+            }
+        ]
+    }
+}
